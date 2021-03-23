10acfe59384ce095cde26d0f7b43a56d192a2fef
Abdulah Pehlic <abdulah.pehlic@gmail.com>
2021-03-23 22:06:37 +0100

    Spring Boot app setup

---
+++ src/test/java/com/vaudience/contactio/ContactioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaudience.contactio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
